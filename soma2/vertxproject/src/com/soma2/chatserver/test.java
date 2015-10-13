940299025a58a40c0daaacf87e76c559f3995d86
이한별 <hbyullee90@gmail.com>
2015-10-13 17:29:54 +0900

    a

--- soma2/vertxproject/src/com/soma2/chatserver/test.java
+++ soma2/vertxproject/src/com/soma2/chatserver/test.java
@@ -1,16 +1,35 @@
 package com.soma2.chatserver;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class test {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			Connection con = null;
 
-		
-		String a= "kkk";
-		String b= "kkk";
-		
-		System.out.println(a==b);
-		
+			con = DriverManager.getConnection("jdbc:mysql://localhost",
+					"root", "1234567890");
+
+			java.sql.Statement st = null;
+			ResultSet rs = null;
+			st = con.createStatement();
+			rs = st.executeQuery("SHOW DATABASES");
+
+			if (st.execute("SHOW DATABASES")) {
+				rs = st.getResultSet();
+			}
+
+			while (rs.next()) {
+				String str = rs.getNString(1);
+				System.out.println(str);
+			}
+		} catch (SQLException sqex) {
+			System.out.println("SQLException: " + sqex.getMessage());
+			System.out.println("SQLState: " + sqex.getSQLState());
+		}
+
 	}
-
 }
